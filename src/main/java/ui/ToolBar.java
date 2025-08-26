6f562653c59ec1f5b7f07f2f3e6b465fb9444248
forteres <forteres1@hotmail.com>
2025-08-26 20:38:11 -0300

    add toolbar icons

--- src/main/java/ui/ToolBar.java
+++ src/main/java/ui/ToolBar.java
@@ -1,37 +1,46 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ToolBar {
     JToolBar toolBar;
 
+    private static ImageIcon loadIcon(String path, int width, int height) {
+        ImageIcon icon = new ImageIcon(path);
+        Image scaled = icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        return new ImageIcon(scaled);
+    }
+
     public ToolBar() {
+        int iconSize = 24;
+
         toolBar = new JToolBar();
 
-       JButton fileButton = new JButton("F");
-       JButton folderButton = new JButton("P");
-       JButton saveButton = new JButton("S");
-       JButton cutButton = new JButton("A");
-       JButton copyButton = new JButton("C");
-       JButton pasteButton = new JButton("P");
-       JButton buildButton = new JButton("B");
-       JButton runButton = new JButton("R");
-       JButton sosButton = new JButton("L");
+        JButton fileButton   = new JButton(loadIcon("icons/addFile.png", iconSize, iconSize));
+        JButton folderButton = new JButton(loadIcon("icons/folder.png", iconSize, iconSize));
+        JButton saveButton   = new JButton(loadIcon("icons/save.png", iconSize, iconSize));
+        JButton cutButton    = new JButton(loadIcon("icons/cut.png", iconSize, iconSize));
+        JButton copyButton   = new JButton(loadIcon("icons/copy.png", iconSize, iconSize));
+        JButton pasteButton  = new JButton(loadIcon("icons/paste.png", iconSize, iconSize));
+        JButton buildButton  = new JButton(loadIcon("icons/hammer.png", iconSize, iconSize));
+        JButton runButton    = new JButton(loadIcon("icons/play.png", iconSize, iconSize));
+        JButton sosButton    = new JButton(loadIcon("icons/sos.png", iconSize, iconSize));
 
-       toolBar.add(fileButton);
-       toolBar.add(folderButton);
-       toolBar.add(saveButton);
+        toolBar.add(fileButton);
+        toolBar.add(folderButton);
+        toolBar.add(saveButton);
 
-       toolBar.addSeparator();
-       toolBar.add(cutButton);
-       toolBar.add(copyButton);
-       toolBar.add(pasteButton);
+        toolBar.addSeparator();
+        toolBar.add(cutButton);
+        toolBar.add(copyButton);
+        toolBar.add(pasteButton);
 
-       toolBar.addSeparator();
-       toolBar.add(buildButton);
-       toolBar.add(runButton);
+        toolBar.addSeparator();
+        toolBar.add(buildButton);
+        toolBar.add(runButton);
 
-       toolBar.addSeparator();
-       toolBar.add(sosButton);
+        toolBar.addSeparator();
+        toolBar.add(sosButton);
     }
 }
