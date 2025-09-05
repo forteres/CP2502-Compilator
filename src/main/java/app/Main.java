3cf2b347dc3417400d742f58a9e84e3cfd98741e
Jo4ovlm <jo4ovlm@gmail.com>
2025-09-05 10:05:48 -0300

    Feito a chamada do analisador no main, e feito um teste de funcionamento

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,8 +2,31 @@ package app;
 
 import ui.Ui;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
     public static void main(String[] args) {
-        Ui screen = new Ui();
+        //Ui screen = new Ui();
+
+        String texto = "begin teste\n" +
+                "set a\n" +
+                "= 10\n" +
+                "show (\n" +
+                "3.14 ,cont\n" +
+                "\"abrir\n" +
+                ") @";
+
+        InputStream input = new ByteArrayInputStream(texto.getBytes(StandardCharsets.UTF_8));
+
+        Linguagem20252 lexer = new Linguagem20252(input);
+
+        try {
+            Linguagem20252.analisar(input);
+        } catch (ParseException e) {
+            System.out.println(e);
+        }
+
     }
 }
